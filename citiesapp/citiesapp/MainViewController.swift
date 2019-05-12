fc2f62c42102bc06573ac1a321c43976a8785d08
Guilherme Pedriconi <guilhermepedriconi@gmail.com>
2019-05-12 18:11:01 -0300

    [INIT][NAVIGATION][ASYNC][HTTP] Http permission and the simple interaction with views

---
+++ citiesapp/citiesapp/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  citiesapp
+//
+//  Created by Guilherme Pedriconi on 12/05/19.
+//  Copyright © 2019 Pedriconi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    var itemSelected:String! = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(itemSelected!)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
